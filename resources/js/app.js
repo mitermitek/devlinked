7cb194fc52f9e930dc3413ece18ddc682a343c0b
mitermitek <40229021+mitermitek@users.noreply.github.com>
2023-12-23 09:38:19 +0100

    Install Ziggy

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import "../css/app.css";
 import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/vue3";
 import Layout from "./Shared/Layout/Layout.vue";
+import { ZiggyVue } from "ziggy-js";
 
 createInertiaApp({
     resolve: (name) => {
@@ -14,6 +15,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(ZiggyVue)
             .mount(el);
     },
 });
